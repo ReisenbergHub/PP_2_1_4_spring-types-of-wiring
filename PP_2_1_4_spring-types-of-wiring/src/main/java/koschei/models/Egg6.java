40601c465614256ad8c798c2af547988fd33a6ea
Reisenberg <yakovlev.a.official@yandex.ru>
2022-11-13 17:22:33 +0500

    fix metor's comments 1,2. add setter into Egg6 and Needle7 methods

--- PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Egg6.java
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Egg6.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    @Autowired
     private Needle7 needle7;
-
-    Egg6(Needle7 needle7) {this.needle7 = needle7;}
 
     @Override
     public String toString() {
         return ", в яйце иголка " + needle7.toString();
+    }
+
+    @Autowired
+    public void setNeedle7(Needle7 needle7) {
+        this.needle7 = needle7;
     }
 }
